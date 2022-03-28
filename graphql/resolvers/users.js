3dc6997f4ae62857f81a294ca812bae90d093c89
MarianaTorresTorres <mari.torret@gmail.com>
2022-03-28 14:41:26 -0400

    emails send

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,19 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
+const nodemailer = require("nodemailer");
+const nodemailerSendgrid = require("nodemailer-sendgrid");
 
 const { validateRegisterInput } = require("../../util/validators");
 const User = require("../../models/User.js");
 
 require("dotenv").config();
+
+const transport = nodemailer.createTransport(
+  nodemailerSendgrid({
+    apiKey: process.env.SENDGRID_API_KEY,
+  })
+);
 
 module.exports = {
   Query: {
@@ -77,6 +85,23 @@ module.exports = {
         process.env.SECRET,
         { expiresIn: "24h" }
       );
+
+      const url = "https://localhost:5000/confirmation/${token}";
+
+      transport
+        .sendMail({
+          from: "mari.torret@gmail.com",
+          to: res.email,
+          subject: "edYou Confirmation Email",
+          html:
+            "Welcome to edYou, `${user.username}`! Click here to confirm your email: <a href=${url}>`${url}`</a>",
+        })
+        .then(() => {
+          console.log("Confirmation Email sent!");
+        })
+        .catch(() => {
+          console.log("Ah shit, the email didn't send for some reason :(");
+        });
 
       return {
         ...res._doc,
